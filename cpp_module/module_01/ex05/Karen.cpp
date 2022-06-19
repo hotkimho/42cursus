84daaca4c46af371ff212f339b8e5270df2b308e
hokim <rlaghekd777@naver.com>
2022-06-19 19:51:19 +0900

    Update text

--- cpp_module/module_01/ex05/Karen.cpp
+++ cpp_module/module_01/ex05/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: hkim2 <hkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 19:24:29 by hkim2             #+#    #+#             */
-/*   Updated: 2022/06/19 19:43:57 by hkim2            ###   ########.fr       */
+/*   Updated: 2022/06/19 19:50:44 by hkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,26 @@
 
 void Karen::debug(void)
 {
-	std::cout << "[debug] I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl;
+	std::cout << "[debug]" << std::endl << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl;
+	std::cout << std::endl;
 }
 
 void Karen::info(void)
 {
-	std::cout << "[info] I cannot believe adding extra bacon costs more money." << std::endl;
+	std::cout << "[info]" << std::endl << "I cannot believe adding extra bacon costs more money." << std::endl;
+	std::cout << std::endl;
 }
 
 void Karen::warning(void)
 {
-	std::cout << "[warning] I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "[warning]" << std::endl << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << std::endl;
 }
 
 void Karen::error(void)
 {
-	std::cout << "[error] This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[error]" << std::endl << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << std::endl;
 }
 
 void Karen::complain(std::string level)
